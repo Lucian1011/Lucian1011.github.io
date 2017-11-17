d16e965fef00319b3e19a29f0e89f1de35657dac
Lucian <851531206@qq.com>
2017-11-17 17:05:31 +0800

    no message

--- Finance/example.js
+++ Finance/example.js
@@ -23,7 +23,7 @@ var option = {
 
 
 function convert_str_to_json(str){
-    return eval('(' + str + ')')
+    return eval('(' + str + ')');
 }
 
 // 使用刚指定的配置项和数据显示图表。
@@ -65,6 +65,6 @@ myChart.showLoading();
 
 $.get('total_data.json').done(function (total_data){
     myChart.hideLoading();
-    total_data = convert_str_to_json(total_data)
-    myChart.setOption(total_data)
+    total_data = convert_str_to_json(total_data);
+    myChart.setOption(total_data);
 })
