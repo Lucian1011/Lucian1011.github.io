a00193c0bc0d78267d49bbbd74a31b130d3e02a9
Lucian <851531206@qq.com>
2017-11-21 21:21:18 +0800

    add a test_jsonp.js

---
+++ Finance/test_jsonp.js
@@ -0,0 +1,6 @@
+var fund_code = "001986"
+var pageSize = 10
+var url = "http://fundmobapi.eastmoney.com/FundMApi/FundNetDiagram.ashx?deviceid=app_danganye_f10&version=V2.1.0&product=EFund&plat=Iphone&FCODE=" + fund_code + "&pageIndex=1&pageSize=" + pageSize + "&_=1500108520818&callback="
+$.get(url,function (json_result){
+  console.error(json_result);
+})
