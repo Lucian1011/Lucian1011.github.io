429a96afbed0648a8f165ef17dc33321dc9ba604
Lucian <851531206@qq.com>
2017-11-24 20:47:41 +0800

    Just push once, not finished yet

--- Finance/test_jsonp.js
+++ Finance/test_jsonp.js
@@ -1,4 +1,3 @@
-chart_DWJZ_LJJZ_lines
 
 var json_data
 // var fund_code = "001986"
@@ -78,23 +77,35 @@ function get_all_lists(fund_code){
       //   DWJZ_list.push(item.DWJZ)
       //   date_list.push(item.FSRQ)
       // })
+      // 需要把这几个反转一下
+      LJJZ_list.reverse();
+      DWJZ_list.reverse();
+      date_list.reverse();
       resolve();
     })
   })
 }
 
-// 基于准备好的dom，初始化echarts实例
-var chart_DWJZ_LJJZ_lines = echarts.init(document.getElementById('DWJZ_LJJZ_lines'));
-chart_DWJZ_LJJZ_lines.showLoading()
+// 把小数转换成百分数，暂时取三位数
+function toPercent(point){
+    var str = Number(point*100).toFixed(3);
+    // str += "%";
+    return str;
+}
 
-get_all_lists(fund_code).then(function(){
-  console.error("各个列表啊");
-  console.error("LJJZ_list", LJJZ_list);
-  console.error("DWJZ_list", DWJZ_list);
-  console.error("date_list", date_list);
-  LJJZ_list.reverse();
-  DWJZ_list.reverse();
-  date_list.reverse();
+
+
+var chart_DWJZ_LJJZ_lines;
+var chart_LJJZ_gap;
+//创建所有的图表对象
+function init_all_echarts(){
+  chart_DWJZ_LJJZ_lines = echarts.init(document.getElementById('DWJZ_LJJZ_lines'));
+  chart_LJJZ_gap = echarts.init(document.getElementById('LJJZ_gap'));
+  chart_DWJZ_LJJZ_lines.showLoading()
+  chart_LJJZ_gap.showLoading()
+}
+
+function create_DWJZ_LJJZ_lines(){
   chart_DWJZ_LJJZ_lines.hideLoading();
   chart_DWJZ_LJJZ_lines.setOption({
     title: {
@@ -126,34 +137,60 @@ get_all_lists(fund_code).then(function(){
       data: DWJZ_list
     }]
   })
-})
+}
 
-
-// 基于准备好的dom，初始化echarts实例
-var chart_LJJZ_gap = echarts.init(document.getElementById('LJJZ_gap'));
-chart_LJJZ_gap.showLoading()
-
-get_all_lists(fund_code).then(function(){
-  console.error("各个列表啊2");
-  console.error("LJJZ_list2", LJJZ_list);
-  console.error("DWJZ_list2", DWJZ_list);
-  console.error("date_list2", date_list);
-  LJJZ_list.reverse();
-  DWJZ_list.reverse();
-  date_list.reverse();
-
-  chart_LJJZ_gap.hideLoading();
+// 创建 gap_list
+function get_gap_list(gap, min_gap){
   let index;
-  let gap = 1;
-  for(index=1; index<=gap; index++){
-    date_list.shift();  // 去掉第一个元素
+  let LJJZ_list_with_gap = [];
+  for(index=0; index<min_gap; index++){
+    LJJZ_list_with_gap.push(0)
   }
-  LJJZ_list_with_gap = [];
   for(index=0; index<LJJZ_list.length-gap; index++){
     let first = LJJZ_list[index]
     let last = LJJZ_list[index+gap]
-    LJJZ_list_with_gap.push((last-first)/first)
+    LJJZ_list_with_gap.push(toPercent((last-first)/first))
   }
+  return LJJZ_list_with_gap
+}
+
+function create_LJJZ_gap(gap){
+  console.log(typeof gap === 'object');
+  let LJJZ_list_with_gap = [];  // 大的 LJJZ_list_with_gap
+  let legend_list = [];  // legend_list
+  let date_list_clone = date_list.concat();
+  // 如果 gap 是一个数组
+  if(typeof gap === 'object'){
+    // 处理一下日期列表，把 gap 数组里面的最小值拿出来
+    gap_temp = gap.concat();
+    gap_temp.sort();
+    let min_gap = gap_temp[0]
+    let index;
+    for(index=0; index<min_gap; index++){
+      date_list_clone.shift();
+    }
+    // 遍历 gap 根据 gap 处理出不同的 LJJZ_list_with_gap 并加入到大的 LJJZ_list_with_gap 数组
+    for(index=0; index<gap.length; index++){
+      LJJZ_list_with_gap.push({
+        name: "LJJZ gap-" + gap[index],
+        type: "line",
+        data: get_gap_list(gap[index], min_gap)
+      })
+      legend_list.push("gap-"+gap[index])
+    }
+  }else{
+    date_list_clone.shift();
+    LJJZ_list_with_gap.push(get_gap_list(gap))
+    legend_list.push("gap-"+gap)
+  }
+
+  chart_LJJZ_gap.hideLoading();
+  let index;
+  // let gap = 1;
+  for(index=1; index<=gap; index++){
+    date_list.shift();  // 去掉数组内的第一个元素
+  }
+  console.log("legend_list", legend_list);
   // console.log("LJJZ_list_with_gap", LJJZ_list_with_gap);
   // console.log("new date_list", date_list);
   chart_LJJZ_gap.setOption({
@@ -167,19 +204,31 @@ get_all_lists(fund_code).then(function(){
 
     },
     legend: {
-      data:['累计净值 gap']
+      data: legend_list
     },
     xAxis: {
-      data: date_list
+      data: date_list_clone
     },
     yAxis: {
       min: 'dataMin',
       max: 'dataMax'
     },
-    series: [{
-      name: '累计净值_gap' + gap,
-      type: 'line',
-      data: LJJZ_list_with_gap
-    }]
+    series: LJJZ_list_with_gap
+    // series: [{
+    //   name: '累计净值_gap' + gap,
+    //   type: 'line',
+    //   data: LJJZ_list_with_gap
+    // }]
   })
+}
+
+
+init_all_echarts();  // 先创建图表对象
+get_all_lists(fund_code).then(function(){
+  console.error("各个列表啊");
+  console.error("LJJZ_list", LJJZ_list);
+  console.error("DWJZ_list", DWJZ_list);
+  console.error("date_list", date_list);
+  create_DWJZ_LJJZ_lines();
+  create_LJJZ_gap([3, 30]);
 })
